4463844982cf08608186b3e9ade70650820ee0fd
hannesvalk <74541556+hannesvalk@users.noreply.github.com>
2021-02-01 20:21:40 +0200

    Add files via upload

--- dump/kasutajad.d
+++ dump/kasutajad.d
@@ -1,12 +1,17 @@
+1 "Mina ise" "K" "" ""
+2 "Sina ise" "K" "" ""
+3 "Ano Nümus" "K" "" ""
+4 "admin" "A" "" ""
+5 "Teenin Daja" "T" "" ""
 .
 PSC
 filename=kasutajad
-records=0000000000000
+records=0000000000005
 ldbname=raamat
-timestamp=2021/02/01-14:23:36
+timestamp=2021/02/01-19:25:28
 numformat=44,46
 dateformat=mdy-1950
 map=NO-MAP
 cpstream=ISO8859-1
 .
-0000000003
+0000000124
--- dump/raamatud.d
+++ dump/raamatud.d
@@ -1,12 +1,17 @@
+1 "Kevade" "Oskar Luts" "" 02/01/21 "Riiul 1"
+2 "Suvi" "Oskar Luts" "" 02/01/21 "Riiul 1"
+3 "Sügis" "Oskar Luts" "" 02/01/21 "Riiul 1"
+4 "Sõda ja rahu" "Lev Tolstoi" "" 02/01/21 "Riiul 2"
+5 "Vana raamat" "Juhan Liiv" "" 02/01/20 ""
 .
 PSC
 filename=raamatud
-records=0000000000000
+records=0000000000005
 ldbname=raamat
-timestamp=2021/02/01-14:23:36
+timestamp=2021/02/01-19:25:28
 numformat=44,46
 dateformat=mdy-1950
 map=NO-MAP
 cpstream=ISO8859-1
 .
-0000000003
+0000000240
